1762eceb54b6e5454123bf2889777d89cb55eea7
cemil <cemil3456@icloud.com>
2025-05-13 11:24:40 +0200

    stuff done

--- www/index.php
+++ www/index.php
@@ -1,13 +1,22 @@
 <?php
+require "database_connection.php";
 
 $breadcrumb = "Home";
-
-
-require "database_connection.php";
 
 $sql = "SELECT * FROM cards";
 $result = mysqli_query($conn, $sql);
 $pokemon_info = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+if (isset($_GET['type'])){
+    $type = $_GET['type'];
+    $sql = "SELECT * FROM cards WHERE type= 'type";
+    $result = mysqli_query($conn, $sql);
+    $pokemon_info = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+}
+
+
+
 
 ?>
 
@@ -27,6 +36,8 @@ $pokemon_info = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 <body class="bg-gray-100">
     <?php include 'navbar.php'; ?>
+
+    <?php include 'filters.php'; ?>
 
     <!-- Main Content -->
     <div class="w-full">
@@ -53,7 +64,7 @@ $pokemon_info = mysqli_fetch_all($result, MYSQLI_ASSOC);
                         <div class="p-6">
                             <h3 class="mb-2 text-xl font-bold"><?php echo $pokemon["name"]; ?></h3>
                             <p class="mb-4 text-gray-600"><?php echo $pokemon["type"]; ?></p>
-                            <a href="pokemon_detail_simple.php?id=<?php echo $pokemon["id"]; ?>" class="text-blue-600 hover:text-blue-800">Meer informatie →</a>
+                            <a href="pokemon_detail.php?id=<?php echo $pokemon["id"]; ?>" class="text-blue-600 hover:text-blue-800">Meer informatie →</a>
                         </div>
                     </div>
                 <?php } ?>
